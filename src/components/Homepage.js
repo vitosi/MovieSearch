1bb748927a146efd50a09a6d5ee4e7e77a8a206b
Vito S <vrs815@gmail.com>
2021-05-04 18:03:24 -0400

    feat: movie api endpoint added

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Homepage() {
-  return <div>Hello</div>;
+  const [movie, setMovie] = useState();
+  const [movieList, setMovieList] = useState();
+
+  async function movieResults() {
+    try {
+      const results = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY.slice(
+          1,
+          -2
+        )}&language=en-US&query=spiderman&page=1&include_adult=false`
+      );
+      console.log(results);
+      setMovieList(results);
+      console.log(movieList);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <button onClick={movieResults}>{'WORK'}</button>;
 }
 
 export default Homepage;
