ebd5c01858107b0ae04b92fb4f15eb8b70700429
Vito S <vrs815@gmail.com>
2021-05-03 20:52:19 -0400

    inital setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Homepage() {
+  return <div>Hello</div>;
+}
+
+export default Homepage;
