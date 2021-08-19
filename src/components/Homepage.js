1045e788e22946e14b6d4cbe4d6ab2c7e21a45ab
Vito S <vrs815@gmail.com>
2021-08-19 14:43:26 -0400

    feat: search form

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,26 +2,43 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function Homepage() {
-  const [movie, setMovie] = useState();
-  const [movieList, setMovieList] = useState();
+  const [movie, setMovie] = useState('');
+  const [movieList, setMovieList] = useState([]);
 
-  async function movieResults() {
+  function handleChange(e) {
+    e.preventDefault();
+    setMovie(e.target.value);
+    console.log(movie);
+  }
+
+  async function movieResults(e) {
+    e.preventDefault();
     try {
-      const results = await axios.get(
+      const { data: results } = await axios.get(
         `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY.slice(
           1,
           -2
-        )}&language=en-US&query=spiderman&page=1&include_adult=false`
+        )}&language=en-US&query=${movie}&page=1&include_adult=false`
       );
-      console.log(results);
-      setMovieList(results);
+      console.log(results.results);
+      setMovieList(results.results);
       console.log(movieList);
     } catch (error) {
       console.log(error);
     }
   }
 
-  return <button onClick={movieResults}>{'WORK'}</button>;
+  return (
+    <div>
+      <form onSubmit={movieResults}>
+        <label>
+          Movie:
+          <input type="text" value={movie} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Homepage;
